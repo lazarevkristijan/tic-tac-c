324bf86ce4e7e330e8aec902c3ccee22f979ad67
lazarevkristijan <lazarevkristijan@gmail.com>
2024-03-10 12:30:03 +0100

    refactor code

--- TicTacC.c
+++ TicTacC.c
@@ -7,9 +7,9 @@
 void printBoard(char board[3][3]);
 int emptySpaces(char board[3][3]);
 char checkWinner(char board[3][3], char player);
-void clearBuffer();
 void resetGame(char board[3][3]);
 int playerMove(char board[3][3], char player, char *playAgain);
+void askPlayAgain(char board[3][3], char *playAgain);
 
 int main(){
 
@@ -64,23 +64,23 @@ int playerMove(char board[3][3], char player, char *playAgain){
     printf("Player %c make a move\n", player);
 
     do{
-    printf("Choose row: ");
-    if(scanf("%d", &row) != 1){
-        printf("Invalid input. Enter a number \n");
-        while(getchar() != '\n');
-        continue;
-    };
-    
-    printf("Choose col: ");
-    if(scanf("%d", &col) != 1){
-        printf("Invalid input. Enter a number \n");
-        while(getchar() != '\n');
-        continue;
-    };
+        printf("Choose row: ");
+        if(scanf("%d", &row) != 1){
+            printf("Invalid input. Enter a number \n");
+            while(getchar() != '\n');
+            continue;
+        };
+        
+        printf("Choose col: ");
+        if(scanf("%d", &col) != 1){
+            printf("Invalid input. Enter a number \n");
+            while(getchar() != '\n');
+            continue;
+        };
 
-    if(row > 3 || row < 0 || col > 3 || col < 0 || board[row][col] != ' ') {
-        printf("Invalid move\n");
-    }
+        if(row > 3 || row < 0 || col > 3 || col < 0 || board[row][col] != ' ') {
+            printf("Invalid move\n");
+        }
     }while(row > 3 || row < 0 || col > 3 || col < 0 || board[row][col] != ' ');
 
     board[row][col] = player;
@@ -90,25 +90,13 @@ int playerMove(char board[3][3], char player, char *playAgain){
         printf("%c WON!\n", player);
         printf("Play again? (Y/N): ");
 
-        clearBuffer();
-        scanf(" %c", playAgain);
-        *playAgain = toupper(*playAgain);
-        if(*playAgain == 'Y'){
-            resetGame(board);
-            printf("NEW GAME\n");
-        }
+        askPlayAgain(board, playAgain);
         return 1;
     } else if (emptySpaces(board) == 0){
-        printf("TIE!");
+        printf("TIE!\n");
         printf("Play again? (Y/N): ");
 
-        clearBuffer();
-        scanf(" %c", playAgain);
-        *playAgain = toupper(*playAgain);
-        if(*playAgain == 'Y'){
-            resetGame(board);
-            printf("NEW GAME\n");
-        }
+        askPlayAgain(board, playAgain);
         return 1;
     }
     return 0;
@@ -128,37 +116,35 @@ int emptySpaces(char board[3][3]){
 
 char checkWinner(char board[3][3], char player){
 
-    if(board[0][0] != ' ' && board[0][0] == board[0][1] && board[0][0] == board[0][2]){
-        return player;
-    } else if(board[1][0] != ' ' && board[1][0] == board[1][1] && board[1][0] == board[1][2]){
-        return player;
-    } else if(board[2][0] != ' ' && board[2][0] == board[2][1] && board[2][0] == board[2][2]){
-        return player;
-    }else if(board[0][0] == board[1][0] && board[0][0] == board[2][0] && board[0][0] != ' '){
-        return player;
-    } else if(board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[0][1] != ' '){
-        return player;
-    } else if(board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[0][2] != ' '){
-        return player;
-    } else if(board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != ' '){
-        return player;
-    } else if(board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != ' '){
+    if( board[0][0] != ' ' && board[0][0] == board[0][1] && board[0][0] == board[0][2] ||
+        board[1][0] != ' ' && board[1][0] == board[1][1] && board[1][0] == board[1][2] ||
+        board[2][0] != ' ' && board[2][0] == board[2][1] && board[2][0] == board[2][2] ||
+        board[0][0] != ' ' && board[0][0] == board[1][0] && board[0][0] == board[2][0] ||
+        board[0][1] != ' ' && board[0][1] == board[1][1] && board[0][1] == board[2][1] ||
+        board[0][2] != ' ' && board[0][2] == board[1][2] && board[0][2] == board[2][2] ||
+        board[0][0] != ' ' && board[0][0] == board[1][1] && board[0][0] == board[2][2] ||
+        board[0][2] != ' ' && board[0][2] == board[1][1] && board[0][2] == board[2][0] 
+    ){
         return player;
     }
-
     return ' ';
 
 }
 
-void clearBuffer() {
-    int c;
-    while((c = getchar()) != '\n' && c != EOF);
-}
 
 void resetGame(char board[3][3]){
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             board[i][j] = ' ';
         }
+    }
+}
+
+void askPlayAgain(char board[3][3], char *playAgain){
+    scanf(" %c", playAgain);
+    *playAgain = toupper(*playAgain);
+    if(*playAgain == 'Y'){
+        resetGame(board);
+        printf("NEW GAME\n");
     }
 }
